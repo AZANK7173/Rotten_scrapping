5871e08a0c4cd025e258bbec03160ef763b571cf
AZANK7173 <felipeazank@gmail.com>
2022-03-28 10:30:32 -0300

    refactor code st_app

--- st_app.py
+++ st_app.py
@@ -1,40 +1,55 @@
 import streamlit as st
 from scrap_rt import *
 
-st.title('Scrapper de filmes usando Beautiful Soup :popcorn:')
+def create_start_page():
+    st.title('Scrapper de filmes usando Beautiful Soup :popcorn:')
 
-st.write("""
+    st.write("""
 
-Na barra de seleção, escolha o ano e será gerado a tabela dos Top filmes do ano segundo o
- Rotten Tomatoes. Por padrão, a tabela gera os 100 melhores avaliados e com maior polularidade, 
- contudo, para períodos mais antigos, apenas alguns filmes estão presentes no site, sem completar 
- 100 deles.
-""")
+    Na barra de seleção, escolha o ano e será gerado a tabela dos Top filmes do ano segundo o
+    Rotten Tomatoes. Por padrão, a tabela gera os 100 melhores avaliados e com maior polularidade, 
+    contudo, para períodos mais antigos, apenas alguns filmes estão presentes no site, sem completar 
+    100 deles.
+    """)
 
-# Checkbox de seleção do ano
-option = st.selectbox(
-     'Selecione o ano',
-     list(range(2021,1949,-1)))
-
-st.write('You selected:', option)
-
-# Com base no escolhido, cria o link para o scrapper
-link = 'https://www.rottentomatoes.com/top/bestofrt/?year=' + str(option)
+    return 
 
 
-st.write('----------------------')
+def create_checkbox():
+    # Checkbox de seleção do ano
+    option = st.selectbox(
+        'Selecione o ano',
+        list(range(2021,1949,-1)))
 
-# Código que realiza o processo de scrapping
-if st.button('Montar Dataset'):
-    
-    st.write('Isso pode demorar alguns minutinhos (3 no máximo, confia).....')
-    df = get_dataframe(link)
+    st.write('You selected:', option)
 
-    st.write('### Tabela de Filmes')
-    st.dataframe(df)
+    # Com base no escolhido, cria o link para o scrapper
+    link = 'https://www.rottentomatoes.com/top/bestofrt/?year=' + str(option)
 
-    st.download_button('Download CSV',df.to_csv().encode('utf-8'), f"movies_{option}.csv",'text/csv',
-    key='download csv')
+    return link, option
 
-else:
+
+
+def create_dataframe_and_download(link, option):
+    # Código que realiza o processo de scrapping
+    if st.button('Montar Dataset'):
+        
+        st.write('Isso pode demorar alguns minutinhos (3 no máximo, confia).....')
+        df = get_dataframe(link)
+
+        st.write('### Tabela de Filmes')
+        st.dataframe(df)
+
+        st.download_button('Download CSV',df.to_csv().encode('utf-8'), f"movies_{option}.csv",'text/csv',
+        key='download csv')
+
+    else:
+        st.write('----------------------')
+
+    return 
+
+if __name__ == '__main__':
+    create_start_page()
+    link, option = create_checkbox()
     st.write('----------------------')
+    create_dataframe_and_download(link, option)
